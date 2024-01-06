abf8f14ee555e8769cf581b1f2282b5607d2d3b6
Tuan Anh <anhvdt18@gmail.com>
2024-01-06 15:48:30 +0700

    22

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
-import "Your code here";
+import React from "react";
 import useTimer from "./useTimer";
+import { formatTime } from "./formatTime";
 
 function App() {
   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
 
   return (
-    <div className="App container">
-      <h1>Coder Timer</h1>
-      <div className="timer__wrapper">
-        <div className="timer__display">
-          <p>{"Your code here"}</p>
-        </div>
-        <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
-            Stop
-          </button>
-          <button
-            className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
-          >
-            Start
-          </button>
-          <button className="button" onClick={"Your code here"}>
-            Reset
-          </button>
+    <>
+      <div className="App container">
+        <h1>Coder Timer 1</h1>
+        <div className="timer__wrapper">
+          <div className="timer__display">
+            <p>{formatTime(time)}</p>
+          </div>
+          <div className="button__wrapper">
+            <button className="button" onClick={stopTimer}>
+              Stop
+            </button>
+            <button className="button" ref={active} onClick={startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Reset
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Save
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="App container">
+        <h1>Coder Timer 2</h1>
+        <div className="timer__wrapper">
+          <div className="timer__display">
+            <p>{formatTime(time)}</p>
+          </div>
+          <div className="button__wrapper">
+            <button className="button" onClick={stopTimer}>
+              Stop
+            </button>
+            <button className="button" ref={active} onClick={startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Reset
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
